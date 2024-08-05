096a9112ebfe24807b26897a22351ff33da62b10
Franklin <131524766+frank245chikanku@users.noreply.github.com>
2024-08-05 06:53:57 +0200

    route

--- src/app/comments /route.ts
+++ src/app/comments /route.ts
@@ -1,9 +1,13 @@
-
-import { comments } from './data';
 import { NextRequest, NextResponse } from 'next/server';
+import { comments } from './data';
 
-export async function GET() {
-  return NextResponse.json(comments);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get("query")
+  const filteredComments = query  ?
+  comments.filter(comment => comment.text.includes(query)) :
+  comments;
+  return NextResponse.json(filteredComments);
 }
 
 export async function POST(request: NextRequest) {
